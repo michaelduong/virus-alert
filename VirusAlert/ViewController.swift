184433a11c8c78890982738a0760235dfb504ad0
Michael Duong <michael@turntlabs.com>
2020-02-10 00:37:47 -0800

    Initial Commit

---
+++ VirusAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirusAlert
+//
+//  Created by Swift Team Six on 2/10/20.
+//  Copyright © 2020 Turnt Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
